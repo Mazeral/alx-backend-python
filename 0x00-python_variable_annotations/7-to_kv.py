ecacf3d2e8f2943203ccbac7650d25d69950c6f0
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-10 13:54:57 +0200

    task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""
+Module for converting key-value pairs.
+
+This module provides a function for converting a key-value
+pair into a tuple, where the value is squared.
+"""
+
+from typing import Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> tuple[str, Union[int, float]]:
+    """
+    Returns a tuple containing the key and the squared value.
+
+    Args:
+        k (str): The key of the key-value pair.
+        v (Union[int, float]): The value of the key-value pair,
+        which can be an integer or a float.
+
+    Returns:
+        tuple[str, Union[int, float]]: A tuple containing the key
+        and the squared value.
+
+    Examples:
+        >>> to_kv("x", 2)
+        ('x', 4)
+        >>> to_kv("y", 3.0)
+        ('y', 9.0)
+    """
+    return (k, v**2)
