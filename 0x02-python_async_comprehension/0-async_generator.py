b1dcd38736ff8718490eecd958fa2afb0ec222d7
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-15 18:54:02 +0200

    documenting task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -12,7 +12,7 @@ import random
 import asyncio
 
 
-async def async_generator() -> asyncio.AsyncGenerator[float, None]:
+async def async_generator():
     """
     An asynchronous generator function that yields a sequence
     of random numbers.
