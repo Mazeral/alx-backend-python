ace9b751f442edfaa1332679398b8de31542c92d
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-15 19:49:21 +0200

    task 1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""
+Module containing an asynchronous comprehension function.
+
+This module defines a single function, `async_comprehension`,
+which generates a list of random numbers using an asynchronous generator.
+"""
+
+import asyncio
+import random
+from . import async_generator
+
+
+async def async_comprehension() -> list[float]:
+    """
+    An asynchronous comprehension function that generates a list of random
+    numbers.
+
+    This function uses an asynchronous generator to generate a list of random
+    numbers.
+
+    Returns:
+        list[float]: A list of random numbers.
+    """
+    return [x async for x in async_generator.async_generator()]
